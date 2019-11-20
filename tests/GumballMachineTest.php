b3fc7be137d7e3a17439ef5ef3e48678043cceaf
TobysHP <36991491+TobysHP@users.noreply.github.com>
2019-11-20 10:29:37 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -73,10 +73,7 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     }
     public function testInsertC()
     {
-        $max__id1=$this->gumballMachineInstance->GetLastIDC();
         $this->assertContains("good job",$this->gumballMachineInstance->InsertC($this->intitule1,$this->duree1, $this->gumballMachineInstance->GetIdP($this->nom2,$this->prenom2)));
-        $max__id2=$this->gumballMachineInstance->GetLastIDC();
-        $this->assertEquals($max__id1+1,$max__id2);
         
         $max__id1=$this->gumballMachineInstance->GetLastIDC();
         $this->assertContains("good job",$this->gumballMachineInstance->InsertC($this->intitule2,$this->duree2, $this->gumballMachineInstance->GetIdP($this->nom1,$this->prenom1)));
